6f2fc3ff018000865413a5f6e57679005c097ea2
Stefan Thöni <stefan@savvy.ch>
2023-04-28 00:01:05 +0200

    SSO with Quaestur

---
+++ lib/omniauth/oauth2_database.rb
@@ -0,0 +1,40 @@
+class Oauth2Database < OmniAuth::Strategies::OAuth2
+  option :name, 'oauth2'
+
+  def initialize(app, *args, &block)
+    # database lookup
+    config = Setting.get('auth_oauth2_credentials') || {}
+    args[0] = config['client_id']
+    args[1] = config['client_secret']
+    args[2][:client_options] = args[2][:client_options].merge(config.symbolize_keys)
+    super
+  end
+
+  def callback_url
+    full_host + script_name + callback_path
+  end
+
+  uid { raw_info['sub'] }
+
+  info do
+    {
+      uid:        raw_info['sub'],
+      login:      raw_info['sub'],
+      username:   raw_info['name'],
+      email:      raw_info['email'],
+      first_name: raw_info['firstname'],
+      last_name:  raw_info['lastname'],
+    }
+  end
+
+  extra do
+    {
+    'raw_info' => raw_info
+    }
+  end
+
+  def raw_info
+    @raw_info ||= access_token.get('/api/v1/user/profile/').parsed
+  end
+
+end
